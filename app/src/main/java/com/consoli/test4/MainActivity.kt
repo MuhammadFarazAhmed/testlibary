0757869368727c082bb6f242cf8fb20456564ecf
NB-0002\farazahmed <farazahmed@consoliads.com>
2021-10-14 12:44:28 +0500

    next version of LibraryInterface

--- app/src/main/java/com/consoli/test4/MainActivity.kt
+++ app/src/main/java/com/consoli/test4/MainActivity.kt
@@ -2,12 +2,25 @@ package com.consoli.test4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.testlibrary.LibraryInterface
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), LibraryInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+
+    }
+
+    override fun onInitialize() {
+
+    }
+
+    override fun onFailure() {
+
+    }
+
+    override fun onSuccess() {
 
     }
 }
---
+++ myapplication/src/main/java/com/example/testlibrary/LibraryInterface.java
@@ -0,0 +1,10 @@
+package com.example.testlibrary;
+
+public interface LibraryInterface {
+
+     void onInitialize();
+
+     void onFailure();
+
+     void onSuccess();
+}
